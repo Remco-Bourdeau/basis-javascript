386f1fa0ede57a995dfa32aca496d1eb44b30811
Remco <remco.bourdeau@vdabcampus.be>
2022-01-10 14:08:34 +0100

    Geslacht V2 - Hidden

--- js/geslachten.js
+++ js/geslachten.js
@@ -2,17 +2,16 @@
 leesGeslachten();
 const opties = document.querySelectorAll("#links a");
 
-async function leesGeslachten () {
+async function leesGeslachten() {
     const response = await fetch("../js/geslachten.json");
-    if (response.ok){
-        const info = await response.json();
+    if (response.ok) {
+        const personen = await response.json();
         document.getElementById("nietGevonden").hidden = true;
-        maakTabel("allen", info);
+        maakTabel(personen);
         for (const optie of opties) {
             optie.onclick = function () {
-                deleteTabel(document.querySelector("tbody"));
-                const gekliktElement = this.dataset.naam;
-                maakTabel(gekliktElement, info);
+                const gekozenGeslacht = this.dataset.geslacht;
+                controleerGeslacht(gekozenGeslacht);
             }
         }
     } else {
@@ -20,29 +19,37 @@ async function leesGeslachten () {
     }
 }
 
-function maakTabel(gekliktGeslacht, info) {
-    for (const element of info) {
+function maakTabel(personen) {
+    for (const persoon of personen) {
         const tbody = document.querySelector("tbody");
-        if (element.geslacht === gekliktGeslacht || gekliktGeslacht === "allen") {
-            const tr = tbody.insertRow();
-            const naam = tr.insertCell();
-            naam.innerText = element.voornaam;
-            const familienaam = tr.insertCell();
-            familienaam.innerText = element.familienaam;
-            const geslacht = tr.insertCell();
-            const geslachtImg = document.createElement("img");
-            geslachtImg.src = `../img/${element.geslacht}.png`;
-            geslacht.appendChild(geslachtImg);
-            const foto = tr.insertCell();
-            const fotoImg = document.createElement("img");
-            fotoImg.src = `../img/${element.foto}`;
-            foto.appendChild(fotoImg);
-        }
+        const tr = tbody.insertRow();
+        tr.dataset.geslacht = persoon.geslacht;
+        tr.hidden = false;
+        const naam = tr.insertCell();
+        naam.innerText = persoon.voornaam;
+        const familienaam = tr.insertCell();
+        familienaam.innerText = persoon.familienaam;
+        const geslacht = tr.insertCell();
+        const geslachtImg = document.createElement("img");
+        geslachtImg.src = `../img/${persoon.geslacht}.png`;
+        geslacht.appendChild(geslachtImg);
+        const foto = tr.insertCell();
+        const fotoImg = document.createElement("img");
+        fotoImg.src = `../img/${persoon.foto}`;
+        foto.appendChild(fotoImg);
+        
     }
 }
 
-function deleteTabel (element) {
-    while (element.lastChild !== null){
-        element.lastChild.remove();
+function controleerGeslacht(geslacht){
+    for (const row of document.querySelector("tbody").rows) {
+        if (row.dataset.geslacht === geslacht){
+            row.hidden = false;
+        } else {
+            row.hidden = true;
+            if (geslacht === "allen"){
+                row.hidden = false;
+            }
+        }
     }
 }
